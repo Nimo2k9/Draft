edba8f6dc09d1cdd26248e29b5e7f951b985c0a0
Niaz Morshed <148305782+Nimo2k9@users.noreply.github.com>
2026-04-17 02:27:01 +0600

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from auth import sign_up, sign_in
 from utils import detect_foods, get_nutrition, normalize_food
 from supabase_db import insert_meal, get_meals
 
