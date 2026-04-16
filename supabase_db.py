a20a5a94ae72fdaf6dded51d7e8a17d0dc515547
Niaz Morshed <148305782+Nimo2k9@users.noreply.github.com>
2026-04-17 03:05:57 +0600

    Update supabase_db.py

--- supabase_db.py
+++ supabase_db.py
@@ -20,13 +20,14 @@ def insert_meal(food, calories, protein, fat, carbs, user_id):
         "carbs": float(carbs),
         "user_id": str(user_id)   # ✅ force string UUID
     }
- try:
+
+    supabase.table("meals").insert(data).execute()
+     try:
         response = supabase.table("meals").insert(data).execute()
         print("SUCCESS:", response)
     except Exception as e:
         print("ERROR:", e)
         raise e
-    supabase.table("meals").insert(data).execute()
 # -------------------------------
 # FETCH MEALS
 # -------------------------------
