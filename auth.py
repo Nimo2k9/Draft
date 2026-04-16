7f2e42862303f07d71ec932811190c5c4cf7fac5
Niaz Morshed <148305782+Nimo2k9@users.noreply.github.com>
2026-04-17 02:40:53 +0600

    Update auth.py

--- auth.py
+++ auth.py
@@ -12,21 +12,44 @@ def get_client():
 # -------------------------------
 def sign_up(email, password):
     supabase = get_client()
-    return supabase.auth.sign_up({"email": email, "password": password})
+    return supabase.auth.sign_up({
+        "email": email,
+        "password": password
+    })
 
 # -------------------------------
 # LOGIN
 # -------------------------------
 def sign_in(email, password):
     supabase = get_client()
-    return supabase.auth.sign_in_with_password({
+    res = supabase.auth.sign_in_with_password({
         "email": email,
         "password": password
     })
 
+    # ✅ SAVE SESSION
+    st.session_state.session = res.session
+    st.session_state.user = res.user
+
+    return res
+
 # -------------------------------
-# GET USER
+# RESTORE SESSION
 # -------------------------------
-def get_user():
+def restore_session():
     supabase = get_client()
-    return supabase.auth.get_user()
+
+    session = st.session_state.get("session")
+
+    if session:
+        try:
+            supabase.auth.set_session(
+                access_token=session.access_token,
+                refresh_token=session.refresh_token
+            )
+
+            user = supabase.auth.get_user().user
+            st.session_state.user = user
+
+        except:
+            st.session_state.user = None
